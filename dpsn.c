650bba4d8e6eaedc74745c0f53e6c1806c0f4314
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-02-05 14:57:29 -0500

    Parse arguments.

---
+++ dpsn.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Command line arguments */
+static struct {
+	/* the alpha and beta parameters */
+	double alpha, beta;
+	/* the non-uniformity parameter */
+	double K;
+	/* the grid threshold */
+	int Nt;
+	/* global value for epsilon */
+	double eps;
+	/* dataset filename */
+	char *dataset;
+	/* the method */
+	enum {UG, AG, AGS} method;
+	/* random seed */
+	long int seed;
+} args;
+
+static void usage(const char *prg)
+{
+	fprintf(stderr, "Usage: %s ALPHA BETA K NT EPS <u|a|t> DATASET [SEED]\n", prg);
+	exit(EXIT_FAILURE);
+}
+
+
+static void parse_arguments(int argc, char **argv)
+{
+	int i;
+
+	printf("Called with: argc=%d\n", argc);
+	for (i = 0; i < argc; i++)
+		printf("%s ", argv[i]);
+	printf("\n");
+
+	if (argc < 8 || argc > 9)
+		usage(argv[0]);
+	if (sscanf(argv[1], "%lf", &args.alpha) != 1 || args.alpha < 0 || args.alpha >= 1)
+		usage(argv[0]);
+	if (sscanf(argv[2], "%lf", &args.beta) != 1 || args.beta < 0 || args.beta >= 1)
+		usage(argv[0]);
+	if (sscanf(argv[3], "%lf", &args.K) != 1)
+		usage(argv[0]);
+	if (sscanf(argv[4], "%u", &args.Nt) != 1)
+		usage(argv[0]);
+	if (sscanf(argv[5], "%lf", &args.eps) != 1 || args.eps < 0)
+		usage(argv[0]);
+	if (strlen(argv[6]) != 1)
+		usage(argv[0]);
+
+	switch (argv[6][0]) {
+	case 'u': args.method = UG; break;
+	case 'a': args.method = AG; break;
+	case 't': args.method = AGS; break;
+	default: usage(argv[0]);
+	}
+
+	args.dataset = strdup(argv[7]);
+	if (argc == 9) {
+		if (sscanf(argv[8], "%ld", &args.seed) != 1)
+			usage(argv[0]);
+	} else
+		args.seed = 42;
+}
+
+int main(int argc, char **argv)
+{
+	parse_arguments(argc, argv);
+
+	free(args.dataset);
+	return 0;
+}
