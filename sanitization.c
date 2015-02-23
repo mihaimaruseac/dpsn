0a4ec73a91cc68f5e43219a12b045a90c5b3354a
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-02-23 16:42:29 -0500

    Mix all methods in splitting the grid

--- sanitization.c
+++ sanitization.c
@@ -90,9 +90,9 @@ static void sanitize_agt(const struct sensor_network *sn, struct grid *g,
 #endif
 
 static void build_tree(const struct sensor_network *sn, struct grid *g,
-		double alpha, double beta,
+		double alpha, double beta, double gamma,
 		double K, int Nt, int max_depth,
-		struct drand48_data *randbuffer)
+		struct drand48_data *randbuffer, enum method method)
 {
 	double epsilon, Nu, factor;
 	int i;
@@ -100,13 +100,23 @@ static void build_tree(const struct sensor_network *sn, struct grid *g,
 	printf("max_depth = %d, epsilon = %5.2lf\n", max_depth, g->epsilon);
 
 	/* 1. compute noisy values inside the grid */
-	epsilon = alpha * g->epsilon;
+	if (method < AGS)
+		epsilon = gamma * g->epsilon;
+	else
+		epsilon = alpha * g->epsilon;
 	printf("\tUsing epsilon = %5.2lf to compute counts\n", epsilon);
 	grd_compute_noisy(sn, g, epsilon, beta, randbuffer);
 
-	/* 2. compute split */
-	factor = K * beta * (1 - beta) * (1 - alpha); // TODO: compute for the UG, AG and extract as param
+	/* 2. compute split factor */
+	factor = K * beta * (1 - beta);
+	switch(method) {
+	case AG: factor *= alpha; /* no break, need the below too */
+	case UG: factor *= 1 - gamma; break;
+	case AGS: factor *= 1 - alpha; break;
+	}
 	printf("\tSplitting factor: %5.2lf (%5.2lf %5.2lf %5.2lf)\n", factor, K, alpha, beta);
+
+	/* 3. Compute split size */
 	Nu = factor * g->epsilon * (g->n_star.val + g->s_star.val / sn->M);
 	printf("\tSplitting params: n: %5.2lf s: %5.2lf Nu: %5.2lf\n",
 			g->n_star.val, g->s_star.val, Nu);
@@ -114,17 +124,40 @@ static void build_tree(const struct sensor_network *sn, struct grid *g,
 	/* 3. recursion end */
 	if (max_depth == 0 || Nu < 0 || (g->Nu = (int)sqrt(Nu)) < Nt) {
 		g->Nu = 0; // TODO: all 3 methods, UG,AG have Nt but still need work
+#if 0 //UG
+		g->Nu = Nt; // split once anyway
+#endif
+#if 0 //AG
+		g->Nu = Nt;
+#endif
 		return;
 	}
 	printf("\tWill split into: %d\n", g->Nu);
 
-	/* 4. split and recurse */
+	/* 4. split */
 	grd_split_cells(sn, g);
+
+	/* 5. recurse */
 	epsilon = g->epsilon - epsilon;
+#if 0 // UG
+	for (i  = 0; i < g->Nu * g->Nu; i++)
+		grd_compute_noisy(sn, &g->cells[i], epsilon, beta, randbuffer);
+#endif
 	for (i  = 0; i < g->Nu * g->Nu; i++) {
 		g->cells[i].epsilon = epsilon;
-		build_tree(sn, &g->cells[i], alpha, beta, K, Nt,
-				max_depth-1, randbuffer);
+#if 0 // AG
+		grd_compute_noisy(sn, &g->cells[i], alpha * epsilon, beta, randbuffer);
+		Nu = epsilon_1 * K * beta * (1 - beta) * (1 - alpha) * (g->cells[i].n_star.val + g->cells[i].s_star.val / sn->M);
+		if (Nu < 0 || (g->cells[i].Nu = (int)sqrt(Nu)) < Nt)
+			g->cells[i].Nu = Nt;
+		grd_split_cells(sn, &g->cells[i]);
+
+		for (j = 0; j < g->cells[i].Nu * g->cells[i].Nu; j++)
+			grd_compute_noisy(sn, &g->cells[i].cells[j], (1 - alpha) * epsilon, beta, randbuffer);
+	}
+#endif
+		build_tree(sn, &g->cells[i], alpha, beta, gamma, K, Nt,
+				max_depth-1, randbuffer, method);
 	}
 }
 
@@ -146,8 +179,9 @@ void sanitize(const struct sensor_network *sn, struct grid *g,
 	g->epsilon = epsilon;
 
 	/* 2. Split cell, build tree */
-	// TODO: below is only for tree
-	build_tree(sn, g, alpha, beta, K, Nt, max_depth, &randbuffer);
+	if (method != AGS) max_depth = 1; /* constant 1 */
+	build_tree(sn, g, alpha, beta, gamma, K, Nt, max_depth,
+			&randbuffer, method);
 
 #if 0
 	switch (method) {
