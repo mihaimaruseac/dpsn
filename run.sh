d05275bcc8f4da3ea66cca8bbb764fba8e813dea
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-02-23 18:42:03 -0500

    Come back to normal.

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ make clean || exit
 rm -f debug_* || exit
 
 make || exit
-${TOOL} ./dpsn 0.5 0.5 10 3 5.0 t 30 datasets/debug1.dat || exit
+${TOOL} ./dpsn 0.5 0.5 10 3 5.0 t 3 datasets/debug1.dat || exit
 ls debug_* &> /dev/null && ./gen_plots.sh debug_* || exit
