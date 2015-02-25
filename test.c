134c88b52dcccafc0b25ab64f8a32e6ab32fac72
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-02-24 20:45:32 -0500

    Fix printing of test values

--- test.c
+++ test.c
@@ -1,17 +1,18 @@
-#include <assert.h>
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "globals.h"
 #include "test.h"
 #include "sn.h"
 
 #ifndef DEBUG_TEST_GRID_TREE
-#define DEBUG_TEST_GRID_TREE 0
+#define DEBUG_TEST_GRID_TREE 1
 #endif
 
 struct san_measure {
+	int first; /* bits of 1 in the first set (the real values) */
 	int both; /* bits of 1 in the intersection */
 	int either; /* bits of 1 in the union */
 	int flip; /* bits of 1 in the xor */
@@ -20,7 +21,16 @@ struct san_measure {
 
 static void sm_init(struct san_measure *sm)
 {
-	sm->both = sm->either = sm->flip = sm->all = 0;
+	memset(sm, 0, sizeof(*sm));
+}
+
+static void sm_print(const struct san_measure *sm)
+{
+	double jaccard = (sm->both + 0.0) / sm->either;
+	double flip_ratio = (sm->flip + 0.0) / sm->all;
+	flip_ratio = 1 - flip_ratio;
+	printf("%d %d %d %d %d %3.2lf %3.2lf", sm->first, sm->both,
+			sm->either, sm->flip, sm->all, jaccard, flip_ratio);
 }
 
 /* val is 0, 1, 2 or 3 */
@@ -34,6 +44,8 @@ static void sm_parse(struct san_measure *sm, int val)
 		sm->flip++;
 		sm->either++;
 	}
+	if (val > 1)
+		sm->first++;
 }
 
 static int do_test_san_cell(const struct sensor_network *sn, const struct grid *g, double t)
@@ -64,9 +76,9 @@ static int do_test_san_cell(const struct sensor_network *sn, const struct grid *
 	printf(" %d %5.2lf %5.2lf %d", g->n, g->s, rho, rho >= sn->theta);
 	printf(" %5.2lf %5.2lf %5.2lf %d", g->n_star.val, g->s_star.val, rho_star, rho_star >= sn->theta);
 	printf(" %5.2lf %5.2lf %5.2lf %d\n", g->n_bar.val, g->s_bar.val, rho_bar, rho_bar >= sn->theta);
-	printf("  ~~  - ------ ---- -", g->n, g->s, rho, rho >= sn->theta);
-	printf(" %5.2lf %5.2lf ---- -", g->n_star.var, g->s_star.var, rho_star, rho_star >= sn->theta);
-	printf(" %5.2lf %5.2lf ---- -\n", g->n_bar.var, g->s_bar.var, rho_bar, rho_bar >= sn->theta);
+	printf("  ~~  - ------ ---- -");
+	printf(" %5.2lf %5.2lf ---- -", g->n_star.var, g->s_star.var);
+	printf(" %5.2lf %5.2lf ---- -\n", g->n_bar.var, g->s_bar.var);
 #endif
 
 	return ret;
@@ -80,9 +92,8 @@ static void test_san(const struct sensor_network *sn, const struct grid *g,
 
 	if (full_tree || !g->Nu) {
 		r = do_test_san_cell(sn, g, t);
-		sm_parse(star, (r & 0x6) >> 1);
-		sm_parse(bar, ((r & 0x4) >> 1) || (r & 0x1));
-		assert(star->all == bar->all);
+		sm_parse(star, (r & 0x06) >> 1);
+		sm_parse(bar, ((r & 0x04) >> 1) | (r & 0x01));
 	}
 
 	for (i = 0; i < g->Nu * g->Nu; i++)
@@ -96,11 +107,10 @@ void test_san_leaf_only(const struct sensor_network *sn, const struct grid *g, d
 	sm_init(&star);
 	sm_init(&bar);
 	test_san(sn, g, t, 0, &star, &bar);
-	printf("%d %d %d %d %3.2lf %3.2lf | %d %d %d %d %3.2lf %3.2lf\n",
-			star.both, star.either, star.flip, star.all,
-			(star.both + 0.0) / star.either, (star.flip + 0.0) / star.all,
-			bar.both, bar.either, bar.flip, bar.all,
-			(bar.both + 0.0) / bar.either, (bar.flip + 0.0) / bar.all);
+	sm_print(&star);
+	printf(" | ");
+	sm_print(&bar);
+	printf("\n");
 }
 
 void test_san_cell(const struct sensor_network *sn, const struct grid *g, double t)
@@ -110,9 +120,8 @@ void test_san_cell(const struct sensor_network *sn, const struct grid *g, double
 	sm_init(&star);
 	sm_init(&bar);
 	test_san(sn, g, t, 1, &star, &bar);
-	printf("%d %d %d %d %3.2lf %3.2lf | %d %d %d %d %3.2lf %3.2lf\n",
-			star.both, star.either, star.flip, star.all,
-			(star.both + 0.0) / star.either, (star.flip + 0.0) / star.all,
-			bar.both, bar.either, bar.flip, bar.all,
-			(bar.both + 0.0) / bar.either, (bar.flip + 0.0) / bar.all);
+	sm_print(&star);
+	printf(" | ");
+	sm_print(&bar);
+	printf("\n");
 }
