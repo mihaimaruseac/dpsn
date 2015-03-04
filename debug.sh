e7398cd2cf5549d32afa39fa3097e66edc536475
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-04 16:40:22 -0500

    Turn back to debug1 before introducing the final grid.

--- debug.sh
+++ debug.sh
@@ -5,5 +5,5 @@ rm -f debug_* || exit
 
 make || exit
 #./dpsn ALPHA BETA K NT EPS <u GAMMA|a GAMMA|t DEPTH> TTRESH DATASET [SEED]
-${TOOL} ./dpsn 0.5 0.5 10 3 .50 t 3 0.1 datasets/debug_N20000_theta20.dat || exit
+${TOOL} ./dpsn 0.5 0.5 10 3 .50 t 3 0.1 datasets/debug1.dat || exit
 ls debug_* &> /dev/null && ./gen_plots.sh debug_* || exit
