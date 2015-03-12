963c5f8614073d3300a20b378ba5235b4662489f
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-12 13:54:11 -0400

    Heatmaps

--- debug.sh
+++ debug.sh
@@ -7,3 +7,4 @@ make || exit
 #./dpsn ALPHA BETA K NT EPS <u GAMMA|a GAMMA|t DEPTH> TTRESH DATASET [SEED]
 ${TOOL} ./dpsn 0.5 0.5 10 3 .50 u .3 0.1 54 datasets/debug1.dat || exit
 ls debug_tree_grid* &> /dev/null && ./gen_debug_tree_grid_plots.sh debug_tree_grid*
+ls debug_uniform_grid* &> /dev/null && ./gen_debug_uniform_grid_plots.sh debug_uniform_grid*
---
+++ gen_debug_uniform_grid_plots.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+plot_one_cube () {
+    file=$1
+    title=$2
+    ix=$3
+    cat << END | gnuplot
+set term png
+set palette cubehelix
+set output "${file}_${title}.png"
+set title "${title}"
+set view map
+splot "${file}" i ${ix} matrix w image
+END
+}
+
+plot_one () {
+    file=$1
+    plot_one_cube ${file} "n" 0
+    plot_one_cube ${file} "s" 1
+    plot_one_cube ${file} "rho" 2
+    plot_one_cube ${file} "n_star" 3
+    plot_one_cube ${file} "s_star" 4
+    plot_one_cube ${file} "rho_star" 5
+    plot_one_cube ${file} "n_bar" 6
+    plot_one_cube ${file} "s_bar" 7
+    plot_one_cube ${file} "rho_bar" 8
+    plot_one_cube ${file} "shape" 9
+    plot_one_cube ${file} "shape_star" 10
+    plot_one_cube ${file} "shape_bar" 11
+}
+
+for i in $@; do
+    plot_one $i
+done
+
