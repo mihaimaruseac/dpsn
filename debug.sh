e1c5860c0177a03cb3eafc67117fdb69101251a9
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-12 12:34:55 -0400

    Restart debugging the grid tree

--- debug.sh
+++ debug.sh
@@ -6,4 +6,4 @@ rm -f debug_* || exit
 make || exit
 #./dpsn ALPHA BETA K NT EPS <u GAMMA|a GAMMA|t DEPTH> TTRESH DATASET [SEED]
 ${TOOL} ./dpsn 0.5 0.5 10 3 .50 u .3 0.1 54 datasets/debug1.dat || exit
-#ls debug_* &> /dev/null && ./gen_plots.sh debug_* || exit
+ls debug_tree_grid* &> /dev/null && ./gen_debug_tree_grid_plots.sh debug_tree_grid*
