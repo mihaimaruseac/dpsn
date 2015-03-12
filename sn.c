fd1ac808f770fe4403b8feefca47d47cd3da8dcc
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-12 13:54:31 -0400

    More TODOs

--- sn.c
+++ sn.c
@@ -432,6 +432,8 @@ static void lrg_print_val(struct low_res_grid_cell **grid, int xcnt, int ycnt,
 {
 	int i, j;
 
+	// TODO: check ordering
+	// TODO: print coordinates?
 	fprintf(f, "# %s\n", section_label);
 	for (i = 0; i < xcnt; i++) {
 		for (j = 0; j < ycnt; j++)
