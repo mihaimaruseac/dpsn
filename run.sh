d5ea235714da56ba3204d11b3a01af35bf41c77e
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-24 17:15:27 -0400

    Update run.sh to contain new generation plots

--- run.sh
+++ run.sh
@@ -3,39 +3,47 @@
 alphas="0.2 0.5 0.9"
 betas="0.2 0.5 0.9"
 gammas="0.01 0.05 0.2"
-depths="3"
-Ks="5 10"
-Nts="3 4"
-TNts="3 5"
+depths="3 4 5"
+Ks="5 10 100"
+Nts="3 4 5"
+TNts="3 5 7"
 epsilons="0.2 0.5 0.9"
-ttreshes="0.1 0.5"
-seeds="42"
+ttreshes="0.01 0.1 0.5"
+seeds="42 142 100"
+resolutions="1 5 10"
 
 datasetdir="datasets"
-outputdir="output/output.r3"
+outputdir="output/output.r4.unifgrid"
 
 test -d ${outputdir} || mkdir -p ${outputdir} || exit
 
 run_uat () {
     datafile=$1
     outfile=$2
+    outbase=${outfile}
     outfile=${outfile}.out
     meth=$3
     NTargs=$4
     margs=$5
 
     > ${outfile}
-    for alpha in ${alphas}; do
-        for beta in ${betas}; do
-            for K in ${Ks}; do
-                for Nt in ${NTargs}; do
-                    for epsilon in ${epsilons}; do
-                        for marg in ${margs}; do
+    for marg in ${margs}; do
+        for alpha in ${alphas}; do
+            for beta in ${betas}; do
+                for K in ${Ks}; do
+                    for Nt in ${NTargs}; do
+                        for epsilon in ${epsilons}; do
                             for ttresh in ${ttreshes}; do
                                 for seed in ${seeds}; do
-                                    ./dpsn ${alpha} ${beta} $K ${Nt}\
-                                        ${epsilon} ${meth} ${marg}\
-                                        ${ttresh} ${datafile} ${seed} >> ${outfile}
+                                    for resolution in ${resolutions}; do
+                                        ./dpsn ${alpha} ${beta} $K ${Nt}\
+                                            ${epsilon} ${meth} ${marg}\
+                                            ${ttresh} ${resolution}\
+                                            ${datafile} ${seed} >> ${outfile}
+                                        for f in debug_*; do
+                                            mv $f ${outbase}_${f}_${alpha}_${beta}_$K_${Nt}_${epsilon}_${meth}_${marg}_${ttresh}_${resolution}_${seed}.gnpl
+                                        done
+                                    done
                                 done
                             done
                         done
