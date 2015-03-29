598c26c06fe312593a87076ff7a1e1aacdc8133c
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-29 15:00:48 -0400

    Add RESOLUTION in debug.sh commentary

--- debug.sh
+++ debug.sh
@@ -4,7 +4,7 @@ make clean || exit
 rm -f debug_* || exit
 
 make || exit
-#./dpsn ALPHA BETA K NT EPS <u GAMMA|a GAMMA|t DEPTH> TTRESH DATASET [SEED]
+#./dpsn ALPHA BETA K NT EPS <u GAMMA|a GAMMA|t DEPTH> TTRESH RESOLUTION DATASET [SEED]
 ${TOOL} ./dpsn 0.5 0.5 10 3 .50 t 3 0.1 1 datasets/debug_N20000_theta80.dat || exit
 ls debug_tree_grid* &> /dev/null && ./gen_debug_tree_grid_plots.sh debug_tree_grid*
 ls debug_uniform_grid* &> /dev/null && ./gen_debug_uniform_grid_plots.sh debug_uniform_grid*
