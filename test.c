90da8aac5fba3395856057a8aab613148d97dc14
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-03-29 15:04:50 -0400

    Test with noisy threshold around real one

--- test.c
+++ test.c
@@ -49,7 +49,7 @@ static void sm_parse(struct san_measure *sm, int val)
 }
 
 static int do_generic_test_san(double *rho, double *rho_star, double *rho_bar,
-		double t, double theta, double s, double n,
+		double t, double nt, double theta, double s, double n,
 		const struct noisy_val *n_star, const struct noisy_val *s_star,
 		const struct noisy_val *n_bar, const struct noisy_val *s_bar)
 {
@@ -61,17 +61,19 @@ static int do_generic_test_san(double *rho, double *rho_star, double *rho_bar,
 
 	ret |= (*rho >= theta) << 2;
 	/* TODO: might need a different threshold than theta */
-	ret |= (*rho_star >= theta) << 1;
-	ret |= (*rho_bar >= theta) << 0;
+	ret |= (*rho_star >= nt * theta) << 1;
+	ret |= (*rho_bar >= nt * theta) << 0;
 
 	return ret;
 }
 
-static int do_test_san_cell(const struct sensor_network *sn, const struct grid *g, double t)
+static int do_test_san_cell(const struct sensor_network *sn,
+		const struct grid *g, double t, double nt)
 {
 	double rho, rho_star, rho_bar;
-	int ret = do_generic_test_san(&rho, &rho_star, &rho_bar, t, sn->theta,
-			g->s, g->n, &g->n_star, &g->s_star, &g->n_bar, &g->s_bar);
+	int ret = do_generic_test_san(&rho, &rho_star, &rho_bar, t, nt,
+			sn->theta, g->s, g->n,
+			&g->n_star, &g->s_star, &g->n_bar, &g->s_bar);
 
 #if DEBUG_TEST_GRID_TREE
 	printf("%01x ~~ ", ret);
@@ -87,58 +89,51 @@ static int do_test_san_cell(const struct sensor_network *sn, const struct grid *
 }
 
 static void test_san(const struct sensor_network *sn, const struct grid *g,
-		double t, int full_tree,
+		double t, double nt, int full_tree,
 		struct san_measure *star, struct san_measure *bar)
 {
 	int i, r;
 
 	if (full_tree || !g->Nu) {
-		r = do_test_san_cell(sn, g, t);
+		r = do_test_san_cell(sn, g, t, nt);
 		sm_parse(star, (r & 0x06) >> 1);
 		sm_parse(bar, ((r & 0x04) >> 1) | (r & 0x01));
 	}
 
 	for (i = 0; i < g->Nu * g->Nu; i++)
-		test_san(sn, &g->cells[i], t, full_tree, star, bar);
+		test_san(sn, &g->cells[i], t, nt, full_tree, star, bar);
 }
 
 static void do_test_san(const struct sensor_network *sn, const struct grid *g,
-		double t, int full_tree)
+		double t, double nt, int full_tree)
 {
 	struct san_measure star, bar;
 
+	printf("Threshold: %5.2lf | Values: ", nt * sn->theta);
 	sm_init(&star);
 	sm_init(&bar);
-	test_san(sn, g, t, full_tree, &star, &bar);
+	test_san(sn, g, t, nt, full_tree, &star, &bar);
 	sm_print(&star);
 	printf(" | ");
 	sm_print(&bar);
 	printf("\n");
 }
 
-void test_san_leaf_only(const struct sensor_network *sn, const struct grid *g, double t)
-{
-	do_test_san(sn, g, t, 0);
-}
-
-void test_san_cell(const struct sensor_network *sn, const struct grid *g, double t)
-{
-	do_test_san(sn, g, t, 1);
-}
-
-void test_san_shape(struct low_res_grid_cell **grid, int xcnt, int ycnt, double t, double theta)
+static void do_test_san_shape(struct low_res_grid_cell **grid,
+		int xcnt, int ycnt, double t, double nt, double theta)
 {
 	double rho, rho_star, rho_bar;
 	struct san_measure star, bar;
 	int i, j, r;
 
+	printf("Threshold: %5.2lf | Values: ", theta);
 	sm_init(&star);
 	sm_init(&bar);
 
 	for (i = 0; i < xcnt; i++)
 		for (j = 0; j < ycnt; j++) {
-			r = do_generic_test_san(&rho, &rho_star, &rho_bar, t, theta,
-					grid[i][j].s, grid[i][j].n,
+			r = do_generic_test_san(&rho, &rho_star, &rho_bar, t,
+					nt, theta, grid[i][j].s, grid[i][j].n,
 					&grid[i][j].n_star, &grid[i][j].s_star,
 					&grid[i][j].n_bar, &grid[i][j].s_bar);
 			sm_parse(&star, (r & 0x06) >> 1);
@@ -149,4 +144,38 @@ void test_san_shape(struct low_res_grid_cell **grid, int xcnt, int ycnt, double 
 	printf(" | ");
 	sm_print(&bar);
 	printf("\n");
+}
+
+void test_san_leaf_only(const struct sensor_network *sn, const struct grid *g, double t)
+{
+	do_test_san(sn, g, t,  .75, 0);
+	do_test_san(sn, g, t,   .9, 0);
+	do_test_san(sn, g, t,  .95, 0);
+	do_test_san(sn, g, t,    1, 0);
+	do_test_san(sn, g, t, 1.05, 0);
+	do_test_san(sn, g, t, 1.10, 0);
+	do_test_san(sn, g, t, 1.25, 0);
+}
+
+void test_san_cell(const struct sensor_network *sn, const struct grid *g, double t)
+{
+	do_test_san(sn, g, t,  .75, 1);
+	do_test_san(sn, g, t,   .9, 1);
+	do_test_san(sn, g, t,  .95, 1);
+	do_test_san(sn, g, t,    1, 1);
+	do_test_san(sn, g, t, 1.05, 1);
+	do_test_san(sn, g, t, 1.10, 1);
+	do_test_san(sn, g, t, 1.25, 1);
+}
+
+void test_san_shape(struct low_res_grid_cell **grid, int xcnt, int ycnt,
+		double t, double theta)
+{
+	do_test_san_shape(grid, xcnt, ycnt, t,  .75, theta);
+	do_test_san_shape(grid, xcnt, ycnt, t,   .9, theta);
+	do_test_san_shape(grid, xcnt, ycnt, t,  .95, theta);
+	do_test_san_shape(grid, xcnt, ycnt, t,    1, theta);
+	do_test_san_shape(grid, xcnt, ycnt, t, 1.05, theta);
+	do_test_san_shape(grid, xcnt, ycnt, t, 1.10, theta);
+	do_test_san_shape(grid, xcnt, ycnt, t, 1.25, theta);
 }
