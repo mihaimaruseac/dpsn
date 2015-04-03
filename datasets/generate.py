221594dd475249366a1fb5713524692df3b69166
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-04-03 13:57:19 -0400

    Change dataset

--- datasets/generate.py
+++ datasets/generate.py
@@ -7,23 +7,26 @@ xmax = 100
 ymin = 0
 ymax = 100
 M = 100
-theta = 20
-N = 200
-filename = 'debug_N{}_theta{}.dat'.format(N, theta)
+m = 20
+theta = 80
+N = 20000
+filename = 'new_N{}_theta{}.dat'.format(N, theta)
 random.seed(42)
 
 # model of real world
 def model_fun(x, y):
-    a = (x/100 - .75) ** 2 + (y /100 - .65) ** 2
-    a = math.sqrt(a)
-    ret = 90 - 250 * math.sin(a)/(a + 10)
-    assert(0 < ret < M)
+    #a = (x/100 - .75) ** 2 + (y /100 - .65) ** 2
+    #a = math.sqrt(a)
+    #ret = 90 - 250 * math.sin(a)/(a + 10)
+    d = (x - 60) ** 2 + (y - 15) ** 2
+    ret = m + M * math.exp(-d/50)
+    if ret > M: ret = M
     return ret
 
 # don't change below
 def generate_point():
-    x = random.uniform(xmin, xmax)
-    y = random.uniform(ymin, ymax)
+    x = random.uniform(xmin, xmax-1)
+    y = random.uniform(ymin, ymax-1)
     return (x, y, model_fun(x, y))
 
 with open(filename, 'w') as f:
