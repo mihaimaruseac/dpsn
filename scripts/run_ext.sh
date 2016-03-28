0004175773dadd451bf838c6e7e784aa402319ed
Mihai Maruseac <mihai.maruseac@gmail.com>
2016-03-28 16:54:38 -0400

    More changes to the run script

--- scripts/run_ext.sh
+++ scripts/run_ext.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-alpha="0.5"
-beta="0.5"
+alphas="0.2 0.3 0.4 0.5"
+betas="0.2 0.3 0.4 0.5 0.6 0.7 0.8"
 K=10
 Nt=3
 epsilons="0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0"
@@ -11,7 +11,7 @@ resolution=1
 seeds="112288 148445 779888 288613 942277 57111 99852 19158 45622 31234"
 
 datasetdir="datasets"
-outputdir="output/ext"
+outputdir="output/ext2"
 test -d ${outputdir} || mkdir -p ${outputdir} || exit
 
 run () {
@@ -22,13 +22,16 @@ run () {
 
     > ${outfile}
     for epsilon in ${epsilons}; do
-        for seed in ${seeds}; do
-            ./dpsn ${alpha} ${beta} $K ${Nt} ${epsilon} t ${depth}\
-                ${ttresh} ${resolution} ${datafile} ${seed} >> ${outfile}
+        for alpha in ${alphas}; do
+            for beta in ${betas}; do
+                for seed in ${seeds}; do
+                    ./dpsn ${alpha} ${beta} $K ${Nt} ${epsilon} t ${depth} ${ttresh} ${resolution} ${datafile} ${seed} >> ${outfile}
+                done
+            done
         done
     done
 }
 
-for i in ${datasetdir}/*_N20000_theta80.dat; do
+for i in ${datasetdir}/new_N*0000_theta80.dat; do
     run $i
 done
